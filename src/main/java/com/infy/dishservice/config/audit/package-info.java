ad844d953c0871a521d0679fff32aabd367d5db3
default <default>
2019-05-20 18:18:31 +0000

    Adding initial code structure

---
+++ src/main/java/com/infy/dishservice/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.infy.dishservice.config.audit;
---
+++ src/main/java/com/infy/dishservice/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.infy.dishservice.repository;
+
+import com.infy.dishservice.domain.Customer;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Customer entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
